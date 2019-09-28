63bb118bbeb77dad132e3c4f02b93e7852cd72cd
XinhuiChen <1198039762@qq.com>
2019-09-28 23:44:27 +0800

    full

--- miniProgram/pages/detail/detail.js
+++ miniProgram/pages/detail/detail.js
@@ -1,66 +1,28 @@
-// pages/detail/detail.js
-Page({
+const fetch = require('../../utils/fetch')
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    good: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad (options) {
+    fetch(`/buy/record/${options.id}`)
+      .then(res => {
+        this.setData({ good: res.data })
+        wx.setNavigationBarTitle({ title: res.data.title })
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  previewHandle (e) {
+    wx.previewImage({
+      current: e.target.dataset.src,
+      urls: this.data.shop.images
+    })
   }
 })
