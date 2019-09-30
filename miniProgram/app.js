73106d35b5ea032790fec5ac521e6b75a395dd15
XinhuiChen <1198039762@qq.com>
2019-09-30 23:29:14 +0800

    openid

--- miniProgram/app.js
+++ miniProgram/app.js
@@ -1,37 +1,32 @@
 //app.js
+
 App({
-  //const cloud = require('wx-server-sdk'),
+  //const cloud = require('wx-server-sdk')
   config: {
     apiBase: 'http://localhost:8088'
   },
-// // <<<<<<< Updated upstream:app.js
-//   onLaunch: function() {
-//     // 展示本地存储能力
-//     var logs = wx.getStorageSync('logs') || []
-//     logs.unshift(Date.now())
-//     wx.setStorageSync('logs', logs)
-//     wx.cloud.init({
-//       env :'cloudpg-v0ty3',
-      
-//     })
-//   }
-// // =======
-// // >>>>>>> Stashed changes:mimiProgram/app.js
+
+  globalData: {
+    isLogin: false,
+    avatarUrl: "",
+    nickName: "",
+    openId: ''
+  },
 
   /**
    * 查看是否已经授权
    */
-  chackAuth(){
+  chackAuth() {
     var that = this
     wx.getSetting({
-      success(res){
+      success(res) {
         // 查看是否授权
-        if(res.authSetting['scope.userInfo']){
+        if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
-            success(res){
+            success(res) {
               that.globalData.avatarUrl = res.userInfo.avatarUrl,
-              that.globalData.nickName=res.userInfo.nickName,
-              that.globalData.isLogin=true
+                that.globalData.nickName = res.userInfo.nickName,
+                that.globalData.isLogin = true
             }
           })
         }
@@ -41,21 +36,12 @@ App({
 
   onLaunch: function() {
     wx.cloud.init({
-      env:'cloudpg-v0ty3',
+      env: 'cloudpg-v0ty3',
       traceuser: true
     })
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
     // 授权？获取数据：什么都不干
-    this.chackAuth() 
-    
+    this.chackAuth()
+
   },
-  globalData: {
-    isLogin: false,
-    avatarUrl:"",
-    nickName:"",
-  }
 })
--- miniProgram/pages/user/index/index.js
+++ miniProgram/pages/user/index/index.js
@@ -1,4 +1,5 @@
 // pages/user/user.js
+const fetch = require('../../../utils/fetch')
 const app = getApp()
 Page({
 
@@ -7,136 +8,130 @@ Page({
    */
   data: {
     functionList: [{
-        "url":"/pages/user/mine/mine",
+        "url": "/pages/user/mine/mine",
         "title": "我的"
       },
       {
-        "url":"/xx",
+        "url": "/xx",
         "title": "信誉中心"
       },
       {
-        "url":"/pages/user/comment/comment",
+        "url": "/pages/user/comment/comment",
         "title": "评价"
       },
     ],
 
 
     isLogin: false,
-    avatarUrl:"",
-    nickname:"",
+    avatarUrl: "",
+    nickname: "",
   },
 
   /**
    * 点击登录
    */
-clickLogin(e){
-  var that = this
-  if(e.detail.errMsg=="getUserInfo:ok"){
-    that.setData({
-      avatarUrl:e.detail.userInfo.avatarUrl,
-      nickName:e.detail.userInfo.nickName,
-      // islogin 不应该在这
-      isLogin:true
-    })
+  clickLogin(e) {
+    var that = this
+    if (e.detail.errMsg == "getUserInfo:ok") {
+      that.setData({
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        nickName: e.detail.userInfo.nickName,
+        // islogin 不应该在这
+        isLogin: true
+      })
 
-    
-  ////////////////<<<<<<<<<<<<<<在服务器端获取通信秘钥，云开发中停用
-    // wx.login({
-    //   success (res) {
-    //     if (res.code) {
-    //       var code = res.code
-    //       // 获取code2Session
-    //       wx.request({
-    //         // 服务器url
-    //         url:"http://localhost:8081/user/login?code="+code,
-    //         success(res) {
-    //           if(!res.data.errcode){
-    //             that.setData({
-    //               isLogin:true
-    //             })
-    //             console.log("登录成功",res)
-    //           }
-    //           else{
-    //             console.log('登录失败',res)
-    //           }
-    //         }
-    //       })
-    //     } else {
-    //       console.log('登录失败！' + res.errMsg)
-    //     }
-    //   }
-    // })
-///////////////////////>>>>>>>>>>>>
-
-  }
+      wx.login({
+        success: function(res) {
+          console.log(res)
+          fetch('/openid', {
+            code: res.code
+          }).then(res => {
+            console.log(res)
+            app.globalData.openId = res.data.openid
+          }).catch(e => {
+            console.log("登录成功: ", e)
+          })
+        }
+      })
+    }
 
   },
+
   /**
    * 查看是否已经授权
    */
-  chackAuth(){
-    this.setData({
-      avatarUrl:app.globalData.avatarUrl,
-      nickName:app.globalData.nickName,
-      isLogin:app.globalData.isLogin
+  chackAuth() {
+    var that = this
+    wx.getSetting({
+      success(res) {
+        // 查看是否授权
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success(res) {
+              that.globalData.avatarUrl = res.userInfo.avatarUrl,
+                that.globalData.nickName = res.userInfo.nickName,
+                that.globalData.isLogin = true
+            }
+          })
+        }
+      }
     })
   },
 
-   
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.chackAuth()
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
