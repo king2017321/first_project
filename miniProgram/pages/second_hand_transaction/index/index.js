50d18ab6afb3b52eef4538afb13afe6fb921625e
XinhuiChen <1198039762@qq.com>
2019-10-13 22:14:55 +0800

    修改二手

--- miniProgram/pages/second_hand_transaction/index/index.js
+++ miniProgram/pages/second_hand_transaction/index/index.js
@@ -1,3 +1,6 @@
+const fetch = require('../../../utils/fetch')
+const app = getApp()
+
 // pages/trade/trade.js
 Page({
 
@@ -5,76 +8,126 @@ Page({
    * 页面的初始数据
    */
   data: {
-    slides: [{
-      "link":"/xx",
-        "image": "/assets/avatar.png"
-      },
-      {
-        "link": "/xx",
-        "image": "/assets/avatar.png"
-      }, {
-        "link": "/xx",
-        "image": "/assets/avatar.png"
-      }, {
-        "link": "/xx",
-        "image": "/assets/avatar.png"
-      }
-    ]
+    recordBuy: [],
+    recordSell: [],
+    pageIndex: 0,
+    pageSize: 20,
+    hasMore: true,
+    search: false,
+    totalCount: 0,
+    active: 0
   },
 
+
+
+  // /record/sell/{openid}
+  loadSell: function() {
+    return fetch('/record/sell/' + app.globalData.openId)
+      .then(res => {
+        const recordSell = this.data.recordSell.concat(res.data)
+        this.setData({
+          recordSell
+        })
+      })
+  },
+
+  // /record/page/{id}
+  loadBuy: function() {
+    let pageIndex = this.data.pageIndex
+    return fetch('/record/page/' + pageIndex++)
+      .then(res => {
+        const totalCount = parseInt(res.header['Total-Count'])
+        const hasMore = this.data.pageIndex * this.data.pageSize < totalCount
+        const recordBuy = this.data.recordBuy.concat(res.data)
+        this.setData({
+          totalCount,
+          hasMore,
+          pageIndex,
+          recordBuy
+        })
+      })
+  },
+
+  // /record/search/{param}
+  loadSearch: function(param) {
+    return fetch('/record/search/' + param).then(res => {
+      this.setData({
+        recordBuy: res.data,
+        hasMore: false
+      })
+    })
+  },
+
+  loadMore: function() {
+    let active = this.data.active
+    if (active == 0)
+      return this.loadBuy()
+    else if (active == 1)
+      return this.loadSell()
+  },
+
+  previewImage: function(e) {
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.imglist // 需要预览的图片http链接列表  
+    })
+  },
+
+  onChange: function(e) {
+    this.setData({
+      active: e.detail.index
+    })
+    if (this.data.active == 0) {
+      if (this.data.recordBuy == false) {
+        this.loadMore()
+      }
+    } else if (this.data.active == 1) {
+      if (this.data.recordSell == false) {
+        this.loadMore()
+      }
+    }
+  },
+
+  onSearch: function(e) {
+    var inputVal = e.detail
+    if (inputVal != "")
+      this.loadMore(inputVal)
+  },
+
+  onCancel: function() {
+    this.setData({
+      record: [],
+      pageIndex: 0,
+      hasMore: true
+    })
+    this.loadSell()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-
+  onLoad: function() {
+    this.loadMore()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function() {
-
+    if (this.data.active == 0) {
+      this.setData({
+        recordBuy: [],
+        pageIndex: 0,
+        hasMore: true
+      })
+    } else if (this.data.active == 1) {
+      this.setData({
+        recordSell: []
+      })
+    }
+    this.loadMore().then(() => wx.stopPullDownRefresh())
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+    if (this.data.active == 0) {
+      this.loadMore()
+    }
   }
 })
